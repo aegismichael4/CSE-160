c00dfce24ab405774fed0c146ea717cdf6007a88
aegismichael4 <amichae2@ucsc.edu>
2026-02-01 19:33:12 -0800

    more lamb improvements

--- asgn2/LambHead.js
+++ asgn2/LambHead.js
@@ -22,36 +22,55 @@ class LambHead extends Cube {
         //pass the matrix to u_ModelMatrix
         gl.uniformMatrix4fv(u_ModelMatrix, false, transMat.elements);
     
-        // front of cube
-        drawTriangle3D( [0.0,0.0,0.0, 1.0,1.0,0.0, 1.0,0.0,0.0 ]);
-        drawTriangle3D( [0.0,0.0,0.0, 0.0,1.0,0.0, 1.0,1.0,0.0 ]);
+        // front of head
+        drawTriangle3D( [0.0,0.0,0.0, -.5,1.0,0.2, 0.0,2.0,0.0 ]);
+        drawTriangle3D( [0.0,0.0,0.0, 1.0,0.0,0.0, 0.0,2.0,0.0 ]);
+        drawTriangle3D( [1.0,2.0,0.0, 1.0,0.0,0.0, 0.0,2.0,0.0 ]);
+        drawTriangle3D( [1.0,0.0,0.0, 1.8,0.5,0.3, 1.0,0.8,0.0 ]);
+        drawTriangle3D( [1.8,0.9,0.3, 1.8,0.5,0.3, 1.0,0.8,0.0 ]);
+        drawTriangle3D( [1.8,0.9,0.3, 1.0,2.0,0.0, 1.0,0.8,0.0 ]);
+
+        // front top
+        drawTriangle3D( [1.8,0.9,0.3, 1.0,2.0,0.0, 1.8,0.9,0.7 ]);
+        drawTriangle3D( [1.0,2.0,1.0, 1.0,2.0,0.0, 1.8,0.9,0.7 ]);
+
+        // top
+        drawTriangle3D( [0.0,2.0,0.0, 1.0,2.0,0.0, 0.0,2.0,1.0 ]);
+        drawTriangle3D( [1.0,2.0,1.0, 1.0,2.0,0.0, 0.0,2.0,1.0 ]);
+
+        // back top
+        drawTriangle3D( [0.0,2.0,0.0, -.5,1.0,0.2, 0.0,2.0,1.0 ]);
+        drawTriangle3D( [-.5,1.0,0.8, -.5,1.0,0.2, 0.0,2.0,1.0 ]);
 
         // draw other faces slightly darker
         gl.uniform4f(u_FragColor, this.rgba[0]*.9, this.rgba[1]*.9, this.rgba[2]*.9, this.rgba[3]);
 
+        // chin
+        drawTriangle3D( [1.8,0.5,0.7, 1.8,0.5,0.3, 1.8,0.9,0.3 ]);
+        drawTriangle3D( [1.8,0.5,0.7, 1.8,0.9,0.7, 1.8,0.9,0.3 ]);
 
-        // top of cube
-        drawTriangle3D( [0.0,1.0,0.0, 1.0,1.0,0.0, 0.0,1.0,1.0 ]);
-        drawTriangle3D( [1.0,1.0,0.0, 0.0,1.0,1.0, 1.0,1.0,1.0 ]);
-
-        // left side of cube
-        drawTriangle3D( [0.0,0.0,0.0, 0.0,1.0,0.0, 0.0,0.0,1.0 ]);
-        drawTriangle3D( [0.0,1.0,1.0, 0.0,1.0,0.0, 0.0,0.0,1.0 ]);
-
-        // right side of cube
-        drawTriangle3D( [1.0,0.0,0.0, 1.0,1.0,0.0, 1.0,0.0,1.0 ]);
-        drawTriangle3D( [1.0,1.0,1.0, 1.0,1.0,0.0, 1.0,0.0,1.0 ]);
+        // bottom back
+        drawTriangle3D( [0.0,0.0,0.0, 0.0,0.0,1.0, -.5,1.0,0.2 ]);
+        drawTriangle3D( [-.5,1.0,0.8, 0.0,0.0,1.0, -.5,1.0,0.2 ]);
 
         // draw bottom and back faces EVEN darker
         gl.uniform4f(u_FragColor, this.rgba[0]*.8, this.rgba[1]*.8, this.rgba[2]*.8, this.rgba[3]);
+        
+        // under chin
+        drawTriangle3D( [1.0,0.0,0.0, 1.8,0.5,0.3, 1.0,0.0,1.0 ]);
+        drawTriangle3D( [1.8,0.5,0.7, 1.8,0.5,0.3, 1.0,0.0,1.0 ]);
 
-        // back of cube
-        drawTriangle3D( [0.0,0.0,1.0, 1.0,1.0,1.0, 1.0,0.0,1.0 ]);
-        drawTriangle3D( [0.0,0.0,1.0, 0.0,1.0,1.0, 1.0,1.0,1.0 ]);
+        // back of head
+        drawTriangle3D( [0.0,0.0,1.0, -.5,1.0,0.8, 0.0,2.0,1.0 ]);
+        drawTriangle3D( [0.0,0.0,1.0, 1.0,0.0,1.0, 0.0,2.0,1.0 ]);
+        drawTriangle3D( [1.0,2.0,1.0, 1.0,0.0,1.0, 0.0,2.0,1.0 ]);
+        drawTriangle3D( [1.0,0.0,1.0, 1.8,0.5,0.7, 1.0,0.8,1.0 ]);
+        drawTriangle3D( [1.8,0.9,0.7, 1.8,0.5,0.7, 1.0,0.8,1.0 ]);
+        drawTriangle3D( [1.8,0.9,0.7, 1.0,2.0,1.0, 1.0,0.8,1.0 ]);
 
+        // bottom of head
+        drawTriangle3D( [0.0,0.0,0.0, 0.0,0.0,1.0, 0.0,0.0,1.0 ]);
+        drawTriangle3D( [1.0,0.0,1.0, 0.0,0.0,1.0, 0.0,0.0,1.0 ]);
 
-        // bottom of cube
-        drawTriangle3D( [0.0,0.0,0.0, 0.0,0.0,1.0, 1.0,0.0,0.0 ]);
-        drawTriangle3D( [1.0,0.0,1.0, 0.0,0.0,1.0, 1.0,0.0,0.0 ]);
     }
 }
--- asgn2/Sheep.js
+++ asgn2/Sheep.js
@@ -1,36 +1,45 @@
 function setUpSheep() {
 
     animation();
+    
+    const body = makeCube([1,1,1,1], [.6,.3,.3], [-.1,g_bodyHeight,0], [g_bodyRot, 0, 0, 1]);
+    
+    const brThigh1 = makeCube([1,1,1,1], [.2,-.17,.15], [.06,.05,.05], [g_brThighRot - 5, 0, 0, 1], body);
+    const brThigh2 = makeCube([1,1,1,1], [.12,-.22,.15], [.2,-.19,.08], [-60, 0, 0, 1], brThigh1);
+    const brCalf = makeCube([1,1,1,1], [.12,-.22,.1], [.06,-.32,.08], [g_brCalfRot + 10, 0, 0, 1], brThigh1);
+    const brHoof = makeCube([.3,.3,.3,1], [.15,-.1,.13], [0.06,-.25,.05], [-3, 0, 0, 1], brCalf);
 
-    var head = new LambHead();
-    head.rgba = [1,1,1,1];
+    const blThigh1 = makeCube([.95,.95,.95,1], [.2,-.17,.15], [.06,.05,.25], [g_blThighRot - 5, 0, 0, 1], body);
+    const blThigh2 = makeCube([.95,.95,.95,1], [.12,-.22,.15], [.2,-.19,.08], [-60, 0, 0, 1], blThigh1);
+    const blCalf = makeCube([.95,.95,.95,1], [.12,-.22,.1], [.06,-.32,.08], [g_blCalfRot + 10, 0, 0, 1], blThigh1);
+    const blHoof = makeCube([.2,.2,.2,1], [.15,-.1,.13], [0.06,-.25,.05], [-5, 0, 0, 1], blCalf);
+
+    const frThigh1 = makeCube([1,1,1,1], [.15,-.22,.15], [.55,0,.05], [g_frThighRot - 40, 0, 0, 1], body);
+    const frThigh2 = makeCube([1,1,1,1], [.1,-.15,.15], [.08,-.2,.075], [50, 0, 0, 1], frThigh1);
+    const frCalf = makeCube([1,1,1,1], [.1,-.23,.1], [.17,-.3,.08], [g_frCalfRot + 35, 0, 0, 1], frThigh1);
+    const frHoof = makeCube([.3,.3,.3,1], [.13,-.1,.13], [0.05,-.21,.05], [5, 0, 0, 1], frCalf);
+
+    const flThigh1 = makeCube([.95,.95,.95,1], [.15,-.22,.15], [.55,0,.25], [g_flThighRot - 40, 0, 0, 1], body);
+    const flThigh2 = makeCube([.95,.95,.95,1], [.1,-.15,.15], [.08,-.2,.075], [50, 0, 0, 1], flThigh1);
+    const flCalf = makeCube([.95,.95,.95,1], [.1,-.23,.1], [.17,-.3,.08], [g_flCalfRot + 35, 0, 0, 1], flThigh1);
+    const flHoof = makeCube([.2,.2,.2,1], [.13,-.1,.13], [0.05,-.21,.05], [5, 0, 0, 1], flCalf);
+
+    const tail1 = makeCube([1,1,1,1], [.1,-.1,.13], [.05,.25,.15], [g_tail1Rot - 40, 0, 0, 1], body);
+    const tail2 = makeCube([1,1,1,1], [.1,-.15,.13], [.05,-.14,.065], [g_tail2Rot + 10, 0, 0, 1], tail1);
+    const tail3 = makeCube([1,1,1,1], [.1,-.2,.13], [.045,-.2,.065], [g_tail3Rot + 20, 0, 0, 1], tail2);
+
+    const neck = makeCube([1,1,1,1], [.21,.23,.17], [.5,.3,0.16], [g_neckRot - 25, 0, 0, 1], body);
+    
+    const head = new LambHead();
+    head.matrix = neck.getTranslatedMatrix();
+    head.setScale(0.17,0.17,0.25);
+    head.setTranslate(.19,.22,.08);
+    head.setRotation(g_headRot + 25,0,0,1)
     head.render();
 
-    //if (parent) cube.matrix = new Matrix4(parent.getTranslatedMatrix());
-    
-    // const body = makeCube([1,1,1,1], [.6,.3,.3], [-.2,g_bodyHeight,0], [g_bodyRot, 0, 0, 1]);
-    
-    // const brThigh1 = makeCube([1,1,1,1], [.2,-.17,.15], [.06,.05,.05], [g_brThighRot - 5, 0, 0, 1], body);
-    // const brThigh2 = makeCube([1,1,1,1], [.12,-.22,.15], [.2,-.19,.08], [-60, 0, 0, 1], brThigh1);
-    // const brCalf = makeCube([1,1,1,1], [.12,-.22,.1], [.06,-.32,.08], [g_brCalfRot + 10, 0, 0, 1], brThigh1);
-    // const brHoof = makeCube([.3,.3,.3,1], [.15,-.1,.13], [0.06,-.25,.05], [-3, 0, 0, 1], brCalf);
-
-    // const blThigh1 = makeCube([.95,.95,.95,1], [.2,-.17,.15], [.06,.05,.25], [g_blThighRot - 5, 0, 0, 1], body);
-    // const blThigh2 = makeCube([.95,.95,.95,1], [.12,-.22,.15], [.2,-.19,.08], [-60, 0, 0, 1], blThigh1);
-    // const blCalf = makeCube([.95,.95,.95,1], [.12,-.22,.1], [.06,-.32,.08], [g_blCalfRot + 10, 0, 0, 1], blThigh1);
-    // const blHoof = makeCube([.2,.2,.2,1], [.15,-.1,.13], [0.06,-.25,.05], [-5, 0, 0, 1], blCalf);
-
-    // const frThigh1 = makeCube([1,1,1,1], [.15,-.22,.15], [.55,0,.05], [g_frThighRot - 40, 0, 0, 1], body);
-    // const frThigh2 = makeCube([1,1,1,1], [.1,-.15,.15], [.08,-.2,.075], [50, 0, 0, 1], frThigh1);
-    // const frCalf = makeCube([1,1,1,1], [.1,-.23,.1], [.17,-.3,.08], [g_frCalfRot + 35, 0, 0, 1], frThigh1);
-    // const frHoof = makeCube([.3,.3,.3,1], [.13,-.1,.13], [0.05,-.21,.05], [5, 0, 0, 1], frCalf);
-
-    // const flThigh1 = makeCube([.95,.95,.95,1], [.15,-.22,.15], [.55,0,.25], [g_flThighRot - 40, 0, 0, 1], body);
-    // const flThigh2 = makeCube([.95,.95,.95,1], [.1,-.15,.15], [.08,-.2,.075], [50, 0, 0, 1], flThigh1);
-    // const flCalf = makeCube([.95,.95,.95,1], [.1,-.23,.1], [.17,-.3,.08], [g_flCalfRot + 35, 0, 0, 1], flThigh1);
-    // const flHoof = makeCube([.2,.2,.2,1], [.13,-.1,.13], [0.05,-.21,.05], [5, 0, 0, 1], flCalf);
-
-    // const neck = makeCube([1,1,1,1], [.21,.23,.17], [.54,.3,0.16], [-38, 0, 0, 1], body);
+    const rEye = makeCube([.1,.1,.1,1], [.05,.05,.05], [.15,.15,0], [45, 0, 0, 1], head);
+    const lEye = makeCube([.1,.1,.1,1], [.05,.05,.05], [.15,.15,.25], [45, 0, 0, 1], head);
+    const nose = makeCube([0.8,.5,.5,1], [.05,.06,.08], [.35,.14,.13], [60, 0, 0, 1], head);
 }
 
 function makeCube(rgba, scale, translate, rotation, parent) {
@@ -63,6 +72,13 @@ var g_frCalfRot = 0;
 var g_flThighRot = 0;
 var g_flCalfRot = 0;
 
+var g_tail1Rot = 0;
+var g_tail2Rot = 0;
+var g_tail3Rot = 0;
+
+var g_neckRot = 0;
+var g_headRot = 0;
+
 var g_animSpeed = 8;
 
 function animation() {
@@ -70,8 +86,8 @@ function animation() {
 
     const animTime = g_animSpeed * g_seconds;
 
-    g_bodyRot = Math.sin(animTime ) * 15;
-    g_bodyHeight = Math.sin(animTime) * .06;
+    g_bodyRot = Math.sin(animTime ) * 12;
+    g_bodyHeight = Math.sin(animTime) * .08;
 
     g_brThighRot = Math.sin(animTime + 2.5) * 20;
     g_brCalfRot = Math.sin(animTime + 3.5) * 30;
@@ -85,4 +101,10 @@ function animation() {
     g_flThighRot = Math.sin(animTime + 1) * 20 + 20;
     g_flCalfRot = Math.sin(animTime + 1.7) * 30 + 10;
 
+    g_tail1Rot = Math.sin(animTime + 1.2) * -10;
+    g_tail2Rot = Math.sin(animTime + 2.2) * -20 - 20;
+    g_tail3Rot = Math.sin(animTime + 3.2) * -30 - 20;
+
+    g_neckRot = Math.sin(animTime + .5) * 20 - 10;
+    g_headRot = Math.sin(animTime + 1) * 10;
 }
