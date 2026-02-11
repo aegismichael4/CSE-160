4807c3d8c519690c3c42edde9a23728ab0c0d5e8
aegismichael4 <amichae2@ucsc.edu>
2026-02-10 19:50:16 -0800

    basic project setup (not wokring lol)

---
+++ asgn3/Cube.js
@@ -0,0 +1,107 @@
+class Cube {
+
+    constructor() {
+        this.rgba = [1,1,1,1];
+        this.matrix = new Matrix4();
+        this.scale = [1,1,1];
+        this.translate = [0,0,0];
+        this.rotAxis = [0,0,1];
+        this.rot = 0;
+    }
+
+    setScale(x, y, z) {
+        this.scale = [x,y,z];
+    }
+
+    setTranslate(x, y, z) {
+        this.translate = [x,y,z];
+    }
+
+    setRotation(degrees, x, y, z) {
+        this.rot = degrees;
+        this.rotAxis = [x,y,z];
+    }
+
+    getTranslatedMatrix() {
+        const transMat = new Matrix4(this.matrix);
+        transMat.translate(this.scale[0]*-.5 + this.translate[0], this.scale[1]*-.5 + this.translate[1], this.scale[2]*-.5 + this.translate[2]); // center based on scale
+        transMat.rotate(this.rot, this.rotAxis[0], this.rotAxis[1], this.rotAxis[2]);
+        return transMat;
+    }
+
+    render() {
+
+        const transMat = new Matrix4(this.matrix);
+
+        transMat.translate(this.scale[0]*-.5 + this.translate[0], this.scale[1]*-.5 + this.translate[1], this.scale[2]*-.5 + this.translate[2]); // center based on scale
+        transMat.rotate(this.rot, this.rotAxis[0], this.rotAxis[1], this.rotAxis[2]);
+        transMat.scale(this.scale[0], this.scale[1], this.scale[2]);
+
+        this.drawCube(transMat);
+    }
+
+    drawCube(transMat) {
+        // pass in color
+        gl.uniform4f(u_FragColor, this.rgba[0], this.rgba[1], this.rgba[2], this.rgba[3]);
+
+        //pass the matrix to u_ModelMatrix
+        gl.uniformMatrix4fv(u_ModelMatrix, false, transMat.elements);
+    
+        // front of cube
+        drawTriangle3D( [0.0,0.0,0.0, 1.0,1.0,0.0, 1.0,0.0,0.0 ]);
+        drawTriangle3D( [0.0,0.0,0.0, 0.0,1.0,0.0, 1.0,1.0,0.0 ]);
+
+        // draw other faces slightly darker
+        gl.uniform4f(u_FragColor, this.rgba[0]*.9, this.rgba[1]*.9, this.rgba[2]*.9, this.rgba[3]);
+
+
+        // top of cube
+        drawTriangle3D( [0.0,1.0,0.0, 1.0,1.0,0.0, 0.0,1.0,1.0 ]);
+        drawTriangle3D( [1.0,1.0,0.0, 0.0,1.0,1.0, 1.0,1.0,1.0 ]);
+
+        // left side of cube
+        drawTriangle3D( [0.0,0.0,0.0, 0.0,1.0,0.0, 0.0,0.0,1.0 ]);
+        drawTriangle3D( [0.0,1.0,1.0, 0.0,1.0,0.0, 0.0,0.0,1.0 ]);
+
+        // right side of cube
+        drawTriangle3D( [1.0,0.0,0.0, 1.0,1.0,0.0, 1.0,0.0,1.0 ]);
+        drawTriangle3D( [1.0,1.0,1.0, 1.0,1.0,0.0, 1.0,0.0,1.0 ]);
+
+        // draw bottom and back faces EVEN darker
+        // gl.uniform4f(u_FragColor, this.rgba[0]*.8, this.rgba[1]*.8, this.rgba[2]*.8, this.rgba[3]);
+
+        // back of cube
+        drawTriangle3D( [0.0,0.0,1.0, 1.0,1.0,1.0, 1.0,0.0,1.0 ]);
+        drawTriangle3D( [0.0,0.0,1.0, 0.0,1.0,1.0, 1.0,1.0,1.0 ]);
+
+
+        // bottom of cube
+        drawTriangle3D( [0.0,0.0,0.0, 0.0,0.0,1.0, 1.0,0.0,0.0 ]);
+        drawTriangle3D( [1.0,0.0,1.0, 0.0,0.0,1.0, 1.0,0.0,0.0 ]);
+
+    }
+}
+
+function drawTriangle3D(vertices) {
+    // Create a buffer object
+    var vertexBuffer = gl.createBuffer();
+    if (!vertexBuffer) {
+        console.log('Failed to create the buffer object');
+        return -1;
+    }
+
+    // Bind the buffer object to target
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+
+    // Write date into the buffer object
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
+    
+    // Assign the buffer object to a_Position variable
+    gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
+
+    // Enable the assignment to a_Position variable
+    gl.enableVertexAttribArray(a_Position);
+
+    // Draw
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+}
---
+++ asgn3/World.js
@@ -0,0 +1,210 @@
+// TexturedQuad.js (c) 2012 matsuda and kanda
+// Vertex shader program
+var VSHADER_SOURCE =`
+    attribute vec4 a_Position;
+    attribute vec2 a_UV;
+
+    varying vec2 v_UV;
+
+    uniform mat4 u_ModelMatrix;
+    uniform mat4 u_GlobalRotateMatrix;
+    uniform mat4 u_ViewMatrix;
+    uniform mat4 u_ProjectionMatrix;
+
+    void main() {
+    gl_Position = u_ProjectionMatrix * u_ViewMatrix * u_GlobalRotateMatrix * u_ModelMatrix;
+    v_UV = a_UV;
+    }`;
+
+// Fragment shader program
+var FSHADER_SOURCE = `
+    varying vec2 v_UV;
+    
+    uniform sampler2D u_Sampler;
+    uniform vec4 u_FragColor;
+
+    void main() {
+        gl_FragColor = u_FragColor;
+    }`;
+
+// Global Variables
+let canvas;
+let gl;
+let a_Position;
+let a_UV;
+let u_FragColor;
+let u_ModelMatrix;
+let u_GlobalRotateMatrix;
+let u_ProjectionMatrix;
+let u_ViewMatrix;
+
+//#region webgl setup
+
+function setUpWebGL() {
+    // Retrieve <canvas> element
+    canvas = document.getElementById('webgl');
+
+    // Get the rendering context for WebGL
+    gl = canvas.getContext("webgl", { preserveDrawingBuffer: true});
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+
+    gl.enable(gl.DEPTH_TEST);
+}
+
+function connectVariablesToGLSL() {
+    // Initialize shaders
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to intialize shaders.');
+        return;
+    }
+
+    // // Get the storage location of a_Position
+    a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if (a_Position < 0) {
+        console.log('Failed to get the storage location of a_Position');
+        return;
+    }
+
+    // Get the storage location of a_UV
+    a_UV = gl.getAttribLocation(gl.program, 'a_UV');
+    if (a_UV < 0) {
+        console.log('Failed to get the storage location of a_UV');
+        return;
+    }
+
+    // Get the storage location of u_FragColor
+    u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+    if (!u_FragColor) {
+        console.log('Failed to get the storage location of u_FragColor');
+        return;
+    }
+
+    //get the storage location of u_ModelMatrix
+    u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
+    if (!u_ModelMatrix) {
+      console.log("Failed to get the storage location of u_ModelMatrix");
+      return;
+    }
+
+    //get the storage location of u_GlobalRotateMatrix
+    u_GlobalRotateMatrix = gl.getUniformLocation(gl.program, 'u_GlobalRotateMatrix');
+    if (!u_GlobalRotateMatrix) {
+      console.log("Failed to get the storage location of u_GlobalRotateMatrix");
+      return;
+    }
+
+    //get the storage location of u_ViewMatrix
+    u_ViewMatrix = gl.getUniformLocation(gl.program, 'u_ViewMatrix');
+    if (!u_ViewMatrix) {
+      console.log("Failed to get the storage location of u_ViewMatrix");
+      return;
+    }
+
+    //get the storage location of u_ProjectionMatrix
+    u_ProjectionMatrix = gl.getUniformLocation(gl.program, 'u_ProjectionMatrix');
+    if (!u_ProjectionMatrix) {
+      console.log("Failed to get the storage location of u_ProjectionMatrix");
+      return;
+    }
+
+    // Get the storage location of u_Sampler
+    var u_Sampler = gl.getUniformLocation(gl.program, 'u_Sampler');
+    if (!u_Sampler) {
+        console.log('Failed to get the storage location of u_Sampler');
+        return false;
+    }
+
+    // set the initial value for all matrices to identity
+    var identityM = new Matrix4();
+    gl.uniformMatrix4fv(u_ModelMatrix, false, identityM.elements);
+    gl.uniformMatrix4fv(u_ViewMatrix, false, identityM.elements);
+    gl.uniformMatrix4fv(u_ProjectionMatrix, false, identityM.elements);
+    gl.uniformMatrix4fv(u_GlobalRotateMatrix, false, identityM.elements);
+}
+
+function convertCoordinatesEventToGL(ev) {
+  var x = ev.clientX; // x coordinate of a mouse pointer
+  var y = ev.clientY; // y coordinate of a mouse pointer
+  var rect = ev.target.getBoundingClientRect();
+
+  x = ((x - rect.left) - canvas.width/2)/(canvas.width/2);
+  y = (canvas.height/2 - (y - rect.top))/(canvas.height/2);
+
+  return ([x,y]);
+}
+
+//#endregion
+
+//#region HTML Setup
+
+function sendTextToHTML(text, htmlID) {
+  htmlElm = document.getElementById(htmlID);
+  if (!htmlElm) {
+    console.log("Failed to get " + htmlID + " from HTML");
+    return;
+  }
+  htmlElm.innerHTML = text;
+}
+
+//#endregion
+
+// Global Variables
+var g_globalAngle = 0;
+var g_prevMouseX = 0;
+var g_startTime = performance.now() / 1000;
+var g_seconds = 0;
+
+function main() {
+  
+  setUpWebGL();
+  connectVariablesToGLSL();
+//   setUpHTMLElements();
+
+  // Register function (event handler) to be called on a mouse press
+  canvas.onmousedown = click;
+  canvas.onmousemove = click;
+
+  // Specify the color for clearing <canvas>
+  gl.clearColor(1.0, 0.8, 0.75, 1.0);
+
+  tick();
+}
+
+ function click(ev) {
+
+  if (ev.buttons == 1) { // enforce click
+
+    const deltaX = ev.clientX - g_prevMouseX;
+
+    g_globalAngle -= deltaX;
+
+  }
+
+    g_prevMouseX = ev.clientX;
+}
+
+function tick() {
+  g_seconds = (performance.now() / 1000) - g_startTime;
+  renderAllShapes();
+  requestAnimationFrame(tick);
+}
+
+function renderAllShapes() {
+  // check the time at the start of the function
+  var startTime = performance.now();
+
+  // Clear <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  // pass the global rotation into u_GlobalRotateMatrix
+  var globalRotMat = new Matrix4().rotate(g_globalAngle, 0, 1, 0);
+  gl.uniformMatrix4fv(u_GlobalRotateMatrix, false, globalRotMat.elements);
+
+  //setUpSheep();
+
+  var duration = performance.now() - startTime;
+  sendTextToHTML(" ms: " + Math.floor(duration) + " fps: " + Math.floor(1000/duration), "fps");
+}
